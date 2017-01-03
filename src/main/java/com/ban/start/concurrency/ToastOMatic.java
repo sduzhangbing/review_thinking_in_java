a1f1d60116ffb6147983f1621dcdb4ea603a4eae
banzhang <sduzhangbing@163.com>
2017-01-03 19:06:45 +0800

    pipe

---
+++ src/main/java/com/ban/start/concurrency/ToastOMatic.java
@@ -0,0 +1,30 @@
+package com.ban.start.concurrency;
+
+/**
+ * Created by banzhang on 17-1-3.
+ */
+class Toast{
+    public enum Status {DRY,BUTTERED,JAMMED}
+    private Status status = Status.DRY;
+    private final int id;
+
+    public Toast(int id) {
+        this.id = id;
+    }
+    public void butter(){status = Status.BUTTERED;}
+    public void jam(){status = Status.JAMMED;}
+    public Status getStatus(){
+        return status;
+    }
+    public int getId(){
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Toast " + id + ": " + status;
+    }
+}
+
+public class ToastOMatic {
+}
